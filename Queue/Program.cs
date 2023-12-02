af68fd930efc1add5a7b1c1f4fbe9ed840267386
Zihan Yang <ws704603301@gmail.com>
2023-12-02 15:23:37 -0500

    Add code for queue implementation in c sharp using linkedlist

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Queue<T>
+{
+    private LinkedList<T> _list = new LinkedList<T>();
+
+    public void Enqueue(T item)
+    {
+        _list.AddLast(item);
+    }
+
+    public T Dequeue()
+    {
+        if (_list.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        T value = _list.First.Value;
+        _list.RemoveFirst();
+        return value;
+    }
+
+    public T Peek()
+    {
+        if (_list.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        return _list.First.Value;
+    }
+    
+    public bool IsEmpty()
+    {
+        return _list.Count == 0;
+    }
+}
